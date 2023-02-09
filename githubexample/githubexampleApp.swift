5d508207c31a06ab586521ba0255c74a802a6a79
Tyson Walker II <tywalk10@gmail.com>
2023-02-09 16:22:38 -0500

    Initial Commit

---
+++ githubexample/githubexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubexampleApp.swift
+//  githubexample
+//
+//  Created by Tyson Laroyce Walker II on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct githubexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
